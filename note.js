5d938c3e37e8a23d737b6b6889b4502c27bc9ca1
omokehine1 <omokehine1>
2016-05-17 17:11:01 +0100

    new approach

--- note.js
+++ note.js
@@ -1,6 +1,14 @@
-Class NotesApplication {
-	constructor(author, notes) {
-		this.author = author;
-		this.width = width;
+function NotesApplication(author, notes) {
+	this.author = author;
+	this.notes = notes;
+
+	var notes = [];
+
+	this.create = function(note) {
+		notes.push(note);
+	}
+
+	this.listNotes = function() {
+		console.log()
 	}
 }
