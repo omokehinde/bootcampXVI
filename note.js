8703a91f61805f97d7409421b34edf3ed16934ba
omokehine1 <omokehine1>
2016-05-18 08:02:36 +0100

    add listNotes

--- note.js
+++ note.js
@@ -1,14 +1,18 @@
 function NotesApplication(author, notes) {
 	this.author = author;
-	this.notes = notes;
-
-	var notes = [];
+	this.notes = [];
 
 	this.create = function(note) {
 		notes.push(note);
 	}
 
 	this.listNotes = function() {
-		console.log()
+		var index = notes.indexOf(note);
+		if (index = "") {
+			console.log('Error! Type in a number.')
+		}
+		console.log(notes.indexOf(note));
+		console.log(author);
+		console.log(note);
 	}
 }
