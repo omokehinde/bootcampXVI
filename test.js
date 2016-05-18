e820cbdf5f35d7ca1c76124608543c8b9cca4df9
omokehine1 <omokehine1>
2016-05-18 08:44:56 +0100

    test1

--- test.js
+++ test.js
@@ -1,15 +1,16 @@
-function NotesApplication(author, notes) {
-	this.author = author;
-	this.notes = [];
+Class NotesApplication
+	constructor(author, notes) {
+		this.author = author;
+		this.notes = [];
 
 	this.create = function(note) {
 		notes.push(note);
 	}
 
-	this.listNotes = function() {
+	this.listNotes  function() {
 		var index = notes.indexOf(note);
 		if (index = "") {
-			console.log('Error! Type in a number.')
+			console.log('Error! Type in a number.');
 		}
 	}
 	console.log(notes.indexOf(note));
